69abdf973020779c4ec94432d689c61e45ca0f4a
Rohan Sirohi <dk979594@gmail.com>
2024-07-23 17:15:46 +0530

    Added Some changes as expected

--- scripts/update_inventory.py
+++ scripts/update_inventory.py
@@ -1,17 +1,17 @@
 import os
 import yaml
 
-_DEFAULT_PROXMOX_SERVER: str = "proxmox.technoplaza.net"
-_DEFAULT_PROXMOX_SSH_USER: str = "ansible"
+_DEFAULT_PROXMOX_SERVER = "192.168.5.250:8006"
+_DEFAULT_PROXMOX_SSH_USER = "ansible"
 
 
 def main():
-    path_prefix: str = ".." if os.getcwd().endswith("/scripts") else "."
+    path_prefix = ".." if os.getcwd().endswith("scripts") else "."
 
     config_path = f"{path_prefix}/collections/sysengquick/k3s/playbooks/group_vars/all/cluster.yml"
 
     with open(config_path, "r") as f:
-        config: dict = yaml.safe_load(f)
+        config = yaml.safe_load(f)
 
     inventory = {
         "all": {
@@ -46,18 +46,20 @@ def main():
 
     inventory["all"]["children"]["k3s"]["children"]["servers"]["hosts"] = {
         server["name"]: {"ansible_host": server["ip"]}
-        for server in config["cluster_nodes"]["servers"]
+        for server in config["cluster_network"]["servers"]
     }
 
     inventory["all"]["children"]["k3s"]["children"]["workers"]["hosts"] = {
         worker["name"]: {"ansible_host": worker["ip"]}
-        for worker in config["cluster_nodes"]["workers"]
+        for worker in config["cluster_network"]["workers"]
     }
 
     inventory_path = f"{path_prefix}/inventory.yaml"
 
     with open(inventory_path, "w") as f:
         yaml.safe_dump(inventory, f)
+
+    print("Inventory file 'inventory.yaml' generated successfully.")
 
 
 if __name__ == "__main__":
